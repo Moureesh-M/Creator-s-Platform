045d24c59408a6a33a989b70e683a3dc63df2864
Moureesh <mmoureesh@gmail.com>
2026-05-11 14:34:20 +0530

    Implement end-to-end user registration with validation

---
+++ client/src/components/common/CategoryTag.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function CategoryTag({category}){
+  return (
+    <span className="category-tag">{category}</span>
+  )
+}
---
+++ client/src/pages/ViewPost.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function ViewPost(){
+  return (
+    <div>
+      <h1>View Post</h1>
+    </div>
+  )
+}
